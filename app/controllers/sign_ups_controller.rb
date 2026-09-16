d7c0f1a5fa78e843e08f95a0e5732108e41d135d
palashkolk <203395566+palashkolk@users.noreply.github.com>
2026-09-16 10:31:51 +0000

    Adds sign-up for unauthenticated users

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,5 +1,20 @@
 class SignUpsController < ApplicationController
+  unauthenticated_access_only
   def show
     @user=User.new
+  end
+  def create
+    @user=User.new(sign_up_params)
+    if @user.save
+      start_new_session_for(@user)
+      redirect_to root_path
+    else
+      render :show, status: :unpropcessable_entity
+    end
+  end
+
+  private
+  def sign_up_params
+    params.expect(user: [ :first_name, :last_name, :email_address, :password, :password_confirmation ])
   end
 end
